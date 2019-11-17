709679bd8c14ae7e14f43312df7a216cd97925cd
OlegZero13 <oleg.zero13@gmail.com>
2019-11-17 22:44:59 +0100

    Developed validators for Loans model class.

--- pipeline/validations.py
+++ pipeline/validations.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 
 from .models import Borrowers, Loans
 from .models import home_ownership_choices, address_state_choices
+from .models import grade_choices, status_choices, term_choices
 
 import numpy as np
 import string
@@ -42,7 +43,7 @@ class Borrowers_Validation:
     @classmethod
     def validate_home_ownership(cls, value):
         choices = [c[0] for c in home_ownership_choices()]
-        if value.upper not in choices:
+        if value.upper() not in choices:
             return 'OTHER'
         return value.upper()
 
@@ -74,3 +75,68 @@ class Borrowers_Validation:
     def validate_emp_length(cls, value):
         return np.clip(np.int(value), 0, 10)
 
+
+class Loans_Validaton:
+
+    @classmethod
+    def check_fields(cls, request_body):
+        fields = set([f.name for f in Loans._meta.get_fields()])
+        fields.remove('loans')
+        fields.remove('member_id')
+    
+        keys = set(request_body.keys())
+
+        for f in fields:
+            if f not in keys:
+                raise ValidationError(f"'{f}' was not found in {keys}.\n")
+
+    @classmethod
+    def validate_member_id(cls, value):
+        value = int(value)
+        if value < 1:
+            raise ValidationError("Member PK cannto be negative.\n")
+        return value
+
+    @classmethod
+    def validate_title(cls, value):
+        return str(value).lower()
+
+    @classmethod
+    def validate_funded_amount(cls, value):
+        return np.clip(np.float(value), 0.0, 5e4)
+
+    @classmethod
+    def validate_total_payment(cls, value):
+        return np.clip(np.float(value), 0.0, 1e5)
+
+    @classmethod
+    def validate_grade(cls, value):
+        choices = [c[0] for c in grade_choices()]
+        if value.upper() not in choices:
+            raise ValidationError(f"The {value} is not supported.")
+        return value.upper()
+
+    @classmethod
+    def validate_interest_rate(cls, value):
+        return np.clip(np.float(value), 0.0, 32.0)
+
+    @classmethod
+    def validate_amount(cls, value):
+        return np.clip(np.float(value), 0.0, 5e4)
+
+    @classmethod
+    def validate_status(cls, value):
+        choices = [c[0] for c in status_choices()]
+        if value.upper() not in choices:
+            return 'DEFAULT'
+        return value.upper()
+
+    @classmethod
+    def validate_meets_policy(cls, value):
+        return bool(value)
+
+    @classmethod
+    def validate_term(cls, value):
+        if value not in term_choices():
+            raise ValidationError("The term choices supported are only 36 and 60 months.")
+        return value
