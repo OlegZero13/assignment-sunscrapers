b652b32d06b1bb316909e0797fa4a51eefa13a88
OlegZero13 <oleg.zero13@gmail.com>
2019-11-17 12:42:30 +0100

    Created basic reprentation of DB schema.

--- pipeline/models.py
+++ pipeline/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 from itertools import product
+import pandas as pd
+
+
+def load_state_codes():
+    df = pd.read('states_lookup.csv').set_index('Abbreviation')
+    df = df.to_dict()['State']
+    return tuple(zip(df.items()))
 
 
 class Loans(models.Model):
@@ -22,18 +29,25 @@ class Loans(models.Model):
     meets_policy    = models.BooleanField(default=True)
     term            = models.ChoiceField(choices=TERM_CHOICES, default=30)
 
+    class Meta:
+        db_table = 'Loans'
+
 
 class Borrowers(models.Model):
-    HOME_OWNERSHIP_CHOICES = ('MORTGAGE, 'RENT', 'OWN', 'OTHER')
-    
+    HOME_OWNERSHIP_CHOICES = ('MORTGAGE', 'RENT', 'OWN', 'OTHER')
+    STATE_CODE_CHOICES = load_state_codes()
+
     member_id       = models.AutoField(primary_key=True)
     all_util        = models.FloatField(default=0.0)
     annual_income   = models.FloatField(default=0.0)
     avg_cur_bal     = models.FloatField()
     dti             = models.FloatField()
     home_ownership  = models.ChoiceField(choices=HOME_OWNERSHIP_CHOICES, default='OTHER')
-    address_staet   = models.ChoiceField()
+    address_state   = models.ChoiceField(choices=STATE_CODE_CHOICES, default='')
     zip_code        = models.CharField(max_length=3)
     emp_title       = models.CharField(max_length=256)
     emp_length      = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'Borrowers'
 
