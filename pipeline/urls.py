a9f3f532dc5d8f1b7cada89f45c94cb4738aebdd
OlegZero13 <oleg.zero13@gmail.com>
2019-11-17 23:00:21 +0100

    Added url for loan (get, post).

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -7,4 +7,5 @@ app_name = 'pipeline'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^borrower/(?P<member_id>[0-9]+)/$', views.borrower, name='borrower'),
+    url(r'^loan/(?P<loan_id>[0-9]+)/$', views.loan, name='loan'),
 ]
