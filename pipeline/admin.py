947fb73c6ad471a43f2f33c596a0bfa2d50fe5da
OlegZero13 <oleg.zero13@gmail.com>
2019-11-17 21:11:33 +0100

    Added the model represtnation to the admin panel. Next->handle repeating pk.

--- pipeline/admin.py
+++ pipeline/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Borrowers, Loans
 
-# Register your models here.
+
+class Borrowers_AdminPanelDisplay(admin.ModelAdmin):
+    items = set([f.name for f in Borrowers._meta.get_fields()])
+    items.remove('loans')
+    list_display = tuple(items)
+
+class Loans_AdminPanelDisplay(admin.ModelAdmin):
+    list_display = tuple([f.name for f in Loans._meta.get_fields()])
+
+
+admin.site.register(Borrowers, Borrowers_AdminPanelDisplay)
+admin.site.register(Loans, Loans_AdminPanelDisplay)
